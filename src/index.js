37b2d279d449735e2fd6e249dec7e491d190badb
joshticks002 <jadeyemo002@gmail.com>
2022-11-24 18:58:24 +0100

    all CRUD operations

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 const express = require("express");
-const v1Router = require("./v1/routes");
+const bodyParser = require("body-parser");
+const v1WorkoutRouter = require("./v1/routes/workoutRoutes");
 
 const app = express();
-const PORT = process.env.PORT || 3003;
+const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-    res.send("<h2>It's Working!</h2>");
-});
-app.use("/api/v1", v1Router);
+app.use(bodyParser.json());
+app.use("/api/v1/workouts", v1WorkoutRouter);
 
 app.listen(PORT, () => {
     console.log(`API is listening on port ${PORT}`);
