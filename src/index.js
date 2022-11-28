ff7b489653efda79e54cbe0bbe2197e95437bb0a
joshticks002 <jadeyemo002@gmail.com>
2022-11-28 10:52:39 +0100

    cache, routing

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors")
 const v1WorkoutRouter = require("./v1/routes/workoutRoutes");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3003;
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use("/api/v1/workouts", v1WorkoutRouter);
 
--- src/v1/routes/workoutRoutes.js
+++ src/v1/routes/workoutRoutes.js
@@ -1,13 +1,19 @@
 const express = require("express");
-const workoutController = require("../../controllers/workoutController");
+const apicache = require("apicache");
 
+const workoutController = require("../../controllers/workoutController");
+const recordController = require("../../controllers/recordController");
+
+const cache = apicache.middleware;
 const router = express.Router();
 
-router.get("/", workoutController.getAllWorkouts);
+router.get("/", cache("2 minutes"), workoutController.getAllWorkouts);
 
 router.get("/:workoutId", workoutController.getOneWorkout);
 
 router.post("/", workoutController.createNewWorkout);
+
+router.get("/:workoutId/records", recordController.getRecordForWorkout);
 
 router.patch("/:workoutId", workoutController.updateOneWorkout);
 
